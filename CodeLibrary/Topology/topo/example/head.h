cd674ed7d384dc92f3cce0cd293a37c642c44c0b
CedricZhang <zhangce9516@gmail.com>
2020-01-30 19:55:08 +0900

    Initialization

--- CodeLibrary/Topology/topo/example/head.h
+++
@@ -1,8 +0,0 @@
-  NTuple::Item<int>   m_run;
-  NTuple::Item<int>   m_rec;
-
-  NTuple::Item<int>   m_idxmc;
-  NTuple::Array<int>  m_trkidx;
-  NTuple::Array<int>  m_pdgid;
-  NTuple::Array<int>  m_motheridx;
-
